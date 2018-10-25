c9e9d995d366ae352b68b8a8f8a524fe2a94763c
Philipp Elsässer <philipp-elsaesser@outlook.com>
2018-10-25 18:13:17 +0200

    created and provided store. created allReducers.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from './reducers/allReducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(allReducers);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'))
 
